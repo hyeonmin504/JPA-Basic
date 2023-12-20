ee07a72e881db3930171af58155f7fd714211c89
hyunmin504 <hyunmin504@naver.com>
2023-12-21 00:30:33 +0900

    엔티티 맵핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,18 +16,46 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /*
             //영속 = db에 보내기 전에 중간에서 pk, 엔티티, 스냅샷 형태로 1차캐시에 저장되는 공간과 지연 sql저장소
             // jpa가 관리하는 상태 1차 캐시에
-            Member member = new Member(200L, "member200");
-            Member member1 = em.find(Member.class,150L);
+            Member member = new Member(208L, "member208");
             em.persist(member);
-            System.out.println("member1 = " + member1.getName());
 
-            em.flush(); // 쓰기지연 저장소에 정보가 sql에 적용되는 과정
+            Member member1 = em.find(Member.class,208L);
+            System.out.println("member1 = " + member1.getName());
 
             //member.setName("zzzzzz"); //값을 바꾸는 순간 스냅샷이랑 비교해서 업데이트 쿼리문을 작성해둔다
 
             System.out.println("===================");
+             */
+            Member member1 = new Member();
+            member1.setName("A");
+            member1.setAge(17);
+            member1.setRoleType(RoleType.USER);
+
+            Member member2 = new Member();
+            member2.setName("B");
+            member2.setAge(18);
+            member2.setRoleType(RoleType.USER);
+
+            Member member3 = new Member();
+            member3.setName("C");
+            member3.setAge(19);
+            member3.setRoleType(RoleType.ADMIN);
+
+            System.out.println("===================");
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println("member.getId() = " + member1.getId());
+            System.out.println("member2.getId() = " + member2.getId());
+            System.out.println("member3.getId() = " + member3.getId());
+
+            System.out.println("===================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,34 +1,57 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
+import javax.persistence.*;
+import java.util.Date;
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 50)// 50개씩 미리 메모리로 땡겨와서 쓰는 것 성능 최적화
 public class Member {
-    @Id
+    @Id //id만 쓰면 직접 할당
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    private String name;
+    @Column(name = "name", nullable = false) //notnull
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)//기본은 오디널(0,1,2)로, 타입을 스트링으로 바꿔야 추가했을 때 오류가 안난다
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob//varchar을 넘어선 큰 타입
+    private String description;
 
     public Long getId() {
         return id;
     }
 
-    public Member() {}
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
+    public Member() {
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.username = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
